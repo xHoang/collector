50a18c4bc6f1a3d923ec67249139844246471ac6
asasinmode <63049433+asasinmode@users.noreply.github.com>
2021-11-19 09:27:15 +0100

    Update championSelector.vue

--- src/components/championSelector.vue
+++ src/components/championSelector.vue
@@ -107,7 +107,8 @@
          <div class="infoContent" v-else-if="infoIndex === 4">
             <p>Unless it's heavily requested I won't put up the calculation formulas, since making them readable is going to take a lot of time (and most of them aren't pretty looking).</p><br/>
             <p>I verified the damage shown by the site and it seems to be almost <b>100% accurate</b> (in situations where all variables are accounted for), but if you notice something's not right visit the <b>about</b> tab to see where you can contact me.</p><br/>
-            <p>To see what stuff I accounted for in calculations visit the <b>disclaimers</b> tab.</p>
+            <p>To see what stuff I accounted for in calculations visit the <b>disclaimers</b> tab.</p><br/>
+            <p><i>That being said you can look around the source code <a href="https://github.com/asasinmode/collector/" target="_blank">here</a>.</i></p>
          </div>
          <div class="infoContent" v-else-if="infoIndex === 5">
             <p>If you notice something's not working correctly, have any feedback, feature requests or questions contact me on discord <b>asasinmode#0058</b>.</p><br/>
